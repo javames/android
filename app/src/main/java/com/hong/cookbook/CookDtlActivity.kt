b7dbf8be8505cb942aacbb1158475abcde312e84
shichanghong <694684140@qq.com>
2018-02-27 23:12:12 +0800

    home

---
+++ app/src/main/java/com/hong/cookbook/CookDtlActivity.kt
@@ -0,0 +1,14 @@
+package com.hong.cookbook
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by changhong on 2018/2/27.
+ */
+class CookDtlActivity: AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
